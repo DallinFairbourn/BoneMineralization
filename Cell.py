3b120b8669aead29f2e828ab76178269e9c0c833
Dallin Fairbourn <dallin.fairbourn@usu.edu>
2022-11-24 12:27:11 -0600

    Other important files

---
+++ Cell.py
@@ -0,0 +1,7 @@
+
+class Cell:
+    def __init__(self, location):
+        self.location = location
+        
+    def move():
+        # Function to change location
---
+++ Osteoblast.py
@@ -0,0 +1,9 @@
+import numpy as np
+import Cell
+
+class Osteoblast:
+    def __init__(Cell):
+
+    def formCollagen():
+        # function to add collagen to locations within the osteoid
+
